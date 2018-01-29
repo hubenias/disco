e23a80d2e795cad83937c3c5c28536eb879aa82b
Serhii Hubenia <monoxrom@gmail.com>
2018-01-29 05:16:08 +0200

    Let me think I'll implement them in the future(joke).

---
+++ lib/discount/new_year.rb
@@ -0,0 +1,5 @@
+module Discount
+  class NewYear
+    # NOTE: does not implemented, implementation should look similar to Discount::PurchaseHours
+  end
+end
---
+++ lib/discount/total_amount.rb
@@ -0,0 +1,5 @@
+module Discount
+  class TotalAmount
+    # NOTE: does not implemented but way to implement looks similar to Discount::UserLocation
+  end
+end
