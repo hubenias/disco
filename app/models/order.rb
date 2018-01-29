6ab85603bf830b1fb499513fead86450f9f2b357
Serhii Hubenia <monoxrom@gmail.com>
2018-01-29 05:16:59 +0200

    Add calculation for Order.price.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_lines
 
+  # NOTE: created just for convenience, does not covered by specs
+  def update_price
+    self.price = order_lines.map(&:price).reduce(:+)
+    save
+  end
+
 end
--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -3,10 +3,15 @@ class OrderLine < ActiveRecord::Base
   belongs_to :item
 
   before_validation :calculate_price
+  after_save :update_order_price
 
   # NOTE: created just for convenience, does not covered by specs
   def calculate_price
     self.price = item.price * qty if item
+  end
+
+  def update_order_price
+    order.update_price
   end
 
 end
