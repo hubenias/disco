c1025875ad5ed38498343b7e15b9f5d6ba79d650
Serhii Hubenia <monoxrom@gmail.com>
2018-01-29 05:17:35 +0200

    We don't need so many Items in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,8 @@ users = User.create([{ full_name: 'Gregory Skovoroda', location: 'Europe' },
 
 items = Item.create([{ name: 'Ax', price: 120.5 },
                      { name: 'Bag', price: 80.0 },
-                     { name: 'Spade', price: 135.0 },
-                     { name: 'Sopilka', price: 53.0 },
-                     { name: 'Bible', price: 200.0 },
                      { name: 'Boots', price: 105.3 }])
 
-Order.create(user: users.first)
+order = Order.create(user: users.first)
+
+OrderLine.create(order: order, item: items.first, qty: 2)
